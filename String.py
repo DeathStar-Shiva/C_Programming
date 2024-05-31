66fa17650bacfe5546e7697c2e308cefbcc38ebb
Shiva <Shivadharrsan@gmail.com>
2024-05-31 11:29:34 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,16 @@
+str1="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print("Sentence: ")
+print(str1)
+print("In Lower Case: ")
+print(str.lower(str1))
+
+str2="abcdefghijklmnopqrstuvwxyz"
+print("\n\nSentence: ")
+print(str2)
+print("In Upper Case: ")
+print(str.upper(str2))
+
+print("\n\nSentence: ")
+print(str.capitalize(str2))
+print("Length: ")
+print(len(str2))
